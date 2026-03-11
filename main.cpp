195e9faa30975e9212304ba4f8df2f3260ba30d8
Sienna Truong <tqc1234@gmail.com>
2026-03-11 19:09:12 -0400

    init basic classes

--- main.cpp
+++ main.cpp
@@ -1,5 +1,44 @@
 #include <raylib.h>
 
+
+/**
+ * This is what the player will be controlling to shoot skulls
+ */
+class Slingshot
+{
+
+};
+
+
+class Skull
+{   
+};
+
+/**
+ * This is a static skull that will not move
+ */
+class StaticSkull : public Skull
+{
+
+};
+
+/**
+ * This is a skull that is shot by the player
+ */
+class ActiveSkull : public Skull
+{
+
+};
+
+/**
+ * Spawns skulls
+ */
+class Spawner
+{
+
+};
+
+
 int main()
 {
     InitWindow(600, 800, "Strike-A-Pose (but cooler)");
