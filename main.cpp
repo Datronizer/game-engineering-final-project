ad5bf8534b33a0d3b98d9dadb908cd23f38a418e
Sienna Truong <tqc1234@gmail.com>
2026-03-17 17:29:19 -0400

    wip: terrible attempt at refactor
    need dinner before brain works

--- main.cpp
+++ main.cpp
@@ -90,41 +90,7 @@ class SkullsManager;
 class Slingshot;
 
 
-class Skull
-{
-public:
-    SkullColor color;
-    Vector2 position;
 
-    // Default draw if there is no texture (high performance)
-    void Draw(RenderTexture2D skullTexture)
-    {
-        DrawTexture(skullTexture.texture, position.x, position.y, SkullColorToRaylib(color));
-    }
-    // Prefered draw if there is a texture
-    void Draw(Texture2D skullTexture)
-    {
-        // Source rectangle (where the texture is taken from)
-        Rectangle sourceRectangle = {0, 0, TILE_SIZE, TILE_SIZE};
-
-        // Destination rectangle (where the texture is drawn on)
-        Rectangle skullRectangle = {position.x, position.y, SKULL_DIAMETER, SKULL_DIAMETER};
-
-        // Origin of the texture (rotation and scale won't be applied)
-        Vector2 origin = {SKULL_RADIUS, SKULL_RADIUS};
-
-        // Draw the skull
-        DrawTexturePro(skullTexture, sourceRectangle, skullRectangle, origin, 0, SkullColorToRaylib(color));
-    }
-
-    /**
-     * For debugging: returns the skull's color as a string
-     */
-    string ToString()
-    {
-        return "Skull " + to_string(color);
-    }
-};
 
 /**
  * A wall is a special skull that is not affected by the slingshot
@@ -427,6 +393,7 @@ int main()
     skullsManager.Spawn(level);
     skullsManager.LoadRandomSkull(slingshot);
     
+    // Actual texture
     Texture2D skullTexture = LoadTexture("assets/skull_jelly_32x32.png");
 
     // Insane performance hack from Raylib docs (wtf do you mean a texture works better than a circle)
